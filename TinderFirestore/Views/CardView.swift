e5ec07b970f723c8348b03f778446491f302c0e3
franknfjr <frankdonascimentojr@gmail.com>
2019-04-22 11:02:10 -0300

    card view

---
+++ TinderFirestore/Views/CardView.swift
@@ -0,0 +1,29 @@
+//
+//  CardView.swift
+//  TinderFirestore
+//
+//  Created by Frank Ferreira on 22/04/19.
+//  Copyright © 2019 Frank Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIView {
+    
+    fileprivate let imageView = UIImageView(image: #imageLiteral(resourceName: "kelly3"))
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        
+        addSubview(imageView)
+        imageView.fillSuperview()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
