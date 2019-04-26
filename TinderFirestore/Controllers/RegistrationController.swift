e52789841da91c3c174a105b7ecd2120827adbcb
franknfjr <frankdonascimentojr@gmail.com>
2019-04-25 22:42:57 -0300

    criando a screen settings e botany como tela principal provisory

---
+++ TinderFirestore/Controllers/RegistrationController.swift
@@ -0,0 +1,19 @@
+//
+//  RegistrationControllerViewController.swift
+//  TinderFirestore
+//
+//  Created by Frank Ferreira on 25/04/19.
+//  Copyright © 2019 Frank Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+
+    }
+}
