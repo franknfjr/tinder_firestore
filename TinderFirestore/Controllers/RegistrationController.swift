281eb4048a8b5d720dec452ad838518e52f3b0bb
franknfjr <frankdonascimentojr@gmail.com>
2019-04-26 08:38:31 -0300

    custom text field deixando mais limpo as props

--- TinderFirestore/Controllers/RegistrationController.swift
+++ TinderFirestore/Controllers/RegistrationController.swift
@@ -14,25 +14,35 @@ class RegistrationController: UIViewController {
         let button = UIButton()
         button.setTitle("Select Photo", for: .normal)
         button.titleLabel?.font = UIFont.systemFont(ofSize: 32, weight: .heavy)
+        button.backgroundColor = .white
+        button.setTitleColor(.black, for: .normal)
+        button.heightAnchor.constraint(equalToConstant: 270).isActive = true
+        button.layer.cornerRadius = 16
         return button
     }()
     
-    let fullNameTextField: UITextField = {
-        let tf = UITextField()
+    let fullNameTextField: CustomTextField = {
+        let tf = CustomTextField(padding: 24)
         tf.placeholder = "Enter full name"
+        tf.backgroundColor = .white
+        
         return tf
     }()
     
-    let emailTextField: UITextField = {
-        let tf = UITextField()
+    let emailTextField: CustomTextField = {
+        let tf = CustomTextField(padding: 24)
         tf.placeholder = "Enter email"
         tf.keyboardType = .emailAddress
+        tf.backgroundColor = .white
+        
         return tf
     }()
-    let passwordextField: UITextField = {
-        let tf = UITextField()
+    let passwordextField: CustomTextField = {
+        let tf = CustomTextField(padding: 24)
         tf.placeholder = "Enter password"
         tf.isSecureTextEntry = true
+        tf.backgroundColor = .white
+        
         return tf
     }()
     
@@ -48,7 +58,7 @@ class RegistrationController: UIViewController {
         stackView.axis = .vertical
         stackView.spacing = 8
         
-        stackView.anchor(top: nil, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0, left: 32, bottom: 0, right: 32))
+        stackView.anchor(top: nil, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 0, left: 50, bottom: 0, right: 50))
         
         stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
