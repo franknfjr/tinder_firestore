368bd501cce9e546b981ed3a8615fd71d2d2514b
franknfjr <frankdonascimentojr@gmail.com>
2019-05-02 20:17:32 -0300

    salvando imagem

--- TinderFirestore/Controllers/RegistrationViewModel.swift
+++ TinderFirestore/Controllers/RegistrationViewModel.swift
@@ -42,33 +42,36 @@ class RegistrationViewModel {
             
             print("Successfully registered user:", res?.user.uid ?? "")
             
-            let filename = UUID().uuidString
-            let ref_image = Storage.storage().reference(withPath: "/images/\(filename)")
-            let imageData = self.bindableImage.value?.jpegData(compressionQuality: 0.75) ?? Data()
-            
-            ref_image.putData(imageData, metadata: nil, completion: { (_, err) in
-                if let err = err {
-                    completion(err)
-                    return
-                }
-                
-                print("Finished uploading image to Storage")
-                
-                ref_image.downloadURL(completion: { (url, err) in
-                    if let err = err {
-                        completion(err)
-                        return
-                    }
-                    
-                    self.bindableIsRegistering.value = false
-                    print("Download url of our image is:", url?.absoluteString ?? "")
-                    
-                })
-            })
+            self.saveImageToFirebase(completion: completion)
         }
     }
     
     //Reactive programming
     var bindableisFormValid = Bindable<Bool>()
     
+    fileprivate func saveImageToFirebase(completion: @escaping (Error?) -> ()) {
+        let filename = UUID().uuidString
+        let ref_image = Storage.storage().reference(withPath: "/images/\(filename)")
+        let imageData = self.bindableImage.value?.jpegData(compressionQuality: 0.75) ?? Data()
+        
+        ref_image.putData(imageData, metadata: nil, completion: { (_, err) in
+            if let err = err {
+                completion(err)
+                return
+            }
+            
+            print("Finished uploading image to Storage")
+            
+            ref_image.downloadURL(completion: { (url, err) in
+                if let err = err {
+                    completion(err)
+                    return
+                }
+                
+                self.bindableIsRegistering.value = false
+                print("Download url of our image is:", url?.absoluteString ?? "")
+                
+            })
+        })
+    }
 }
