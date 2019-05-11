00af0324702609f6bbbee20527374324bfa3f6f2
franknfjr <frankdonascimentojr@gmail.com>
2019-05-11 20:45:33 -0300

    movimento do like eficiente

--- TinderFirestore/Controllers/HomeController.swift
+++ TinderFirestore/Controllers/HomeController.swift
@@ -107,15 +107,30 @@ class HomeController: UIViewController, SettingsControllerDelegate, LoginControl
     var topCardView: CardView?
     
     @objc fileprivate func handleLike() {
+        let duration = 0.3
+        let translateionAnimation = CABasicAnimation(keyPath: "position.x")
+        translateionAnimation.toValue = 700
+        translateionAnimation.duration = duration
         
-        UIView.animate(withDuration: 1.0, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.1, options: .curveEaseOut, animations: {
-            self.topCardView?.frame = CGRect(x: 600 , y: 0, width: self.topCardView!.frame.width, height: self.topCardView!.frame.height)
-            let angle = 15 * CGFloat.pi / 180
-            self.topCardView?.transform = CGAffineTransform(rotationAngle: angle)
-        }) { (_) in
-            self.topCardView?.removeFromSuperview()
-            self.topCardView = self.topCardView?.nextCardView
+        translateionAnimation.fillMode = .forwards
+        translateionAnimation.timingFunction = CAMediaTimingFunction(name: .easeOut)
+        translateionAnimation.isRemovedOnCompletion = false
+        
+        let rotationAnimation = CABasicAnimation(keyPath: "transform.rotation.z")
+        rotationAnimation.toValue = 15 * CGFloat.pi / 180
+        rotationAnimation.duration = duration
+        
+        let cardView = topCardView
+        topCardView = cardView?.nextCardView
+        
+        CATransaction.setCompletionBlock {
+            cardView?.removeFromSuperview()
         }
+        
+        cardView?.layer.add(translateionAnimation, forKey: "translation")
+        cardView?.layer.add(rotationAnimation, forKey: "rotation")
+        
+        CATransaction.commit()
     }
     
     
